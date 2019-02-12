013a0e4ff09bed0fe430431084c97f565454b4d6
이강현 <khlee92@station3.co.kr>
2019-02-12 17:34:21 +0900

    mobile hamburger sns link issue 수정

--- src/components/Menus/SideMenu/index.js
+++ src/components/Menus/SideMenu/index.js
@@ -1,7 +1,8 @@
 import React from 'react'
-import { Link } from "gatsby"
+import { Link as GatsbyLink } from "gatsby"
 
 import withController from './Controller'
+import { NAVER, INSTAGRAM, FACEBOOK } from 'utils/config'
 
 import Portal from 'utils/portal'
 
@@ -11,6 +12,28 @@ import {
   Wrap, Menus, 
   Footer, Label, SNS, Text,
 } from './styled'
+
+const Link = ({ children, to, activeClassName, ...other }) => {
+  // Tailor the following test to your environment.
+  // This example assumes that any internal link (intended for Gatsby)
+  // will start with exactly one slash, and that anything else is external.
+  const internal = /^\/(?!\/)/.test(to)
+  
+  // Use Gatsby Link for internal links, and <a> for others
+  if (internal) {
+    return (
+      <GatsbyLink to={to} activeClassName={activeClassName} {...other}>
+        {children}
+      </GatsbyLink>
+    )
+  } else {
+    return (
+      <a href={to} {...other}>
+        {children}
+      </a>
+    )
+  }
+}
 
 const SideMenuComponent = ({
   LogoIcon, links,
@@ -59,33 +82,33 @@ const SideMenuComponent = ({
 
             <SNS>
               <li>
-                <a href="/" target="_blank" rel="noreferrer noopener">
+                <Link to={NAVER} target="_blank" rel="noreferrer noopener">
                   <svg viewBox="0 0 48 48">
                     <g fill="none" fillRule="evenodd">
                       <rect width="48" height="48" fill="#FFF" opacity=".7" rx="6.72"/>
                       <path fill="#222" d="M36.121 36.507V12.279h-7.666v12.378l-8.983-12.378h-7.193v24.228h8.08V24.393l8.096 12.114z"/>
                     </g>
                   </svg>
-                </a>
+                </Link>
               </li>
 
               <li>
-                <a href="/" target="_blank" rel="noreferrer noopener">
+                <Link to={FACEBOOK} target="_blank" rel="noreferrer noopener">
                   <svg viewBox="0 0 43 43">
                     <path fill="#FFF" fillRule="evenodd" d="M5.375 0A5.384 5.384 0 0 0 0 5.375v32.25A5.384 5.384 0 0 0 5.375 43H21.5V28.219h-5.375V21.5H21.5v-5.375a8.065 8.065 0 0 1 8.063-8.063h5.375v6.72H32.25c-1.48 0-2.688-.138-2.688 1.343V21.5h6.72l-2.688 6.719h-4.032V43h8.063A5.384 5.384 0 0 0 43 37.625V5.375A5.384 5.384 0 0 0 37.625 0H5.375z" opacity=".7"/>
                   </svg>
-                </a>
+                </Link>
               </li>
 
               <li>
-                <a href="/" target="_blank" rel="noreferrer noopener">
+                <Link to={INSTAGRAM} target="_blank" rel="noreferrer noopener">
                   <svg viewBox="0 0 43 43">
                     <g fill="none" fillRule="evenodd">
                       <path fill="#FFF" d="M38.969 29.563c0 5.186-4.22 9.406-9.407 9.406H13.438c-5.186 0-9.406-4.22-9.406-9.407V13.438c0-5.186 4.22-9.406 9.407-9.406h16.124c5.187 0 9.407 4.22 9.407 9.407v16.124zM29.562 0H13.438C6.016 0 0 6.015 0 13.438v16.124C0 36.986 6.015 43 13.438 43h16.124C36.986 43 43 36.985 43 29.562V13.438C43 6.016 36.985 0 29.562 0z" opacity=".7"/>
                       <path fill="#CCC" d="M21.5 28.219a6.725 6.725 0 0 1-6.719-6.719 6.725 6.725 0 0 1 6.719-6.719 6.725 6.725 0 0 1 6.719 6.719 6.725 6.725 0 0 1-6.719 6.719m0-17.469A10.744 10.744 0 0 0 10.75 21.5c0 5.942 4.808 10.75 10.75 10.75s10.75-4.808 10.75-10.75-4.808-10.75-10.75-10.75M34.042 10.743a1.792 1.792 0 1 1 0 0"/>
                     </g>
                   </svg>
-                </a>
+                </Link>
               </li>
             </SNS>
 
