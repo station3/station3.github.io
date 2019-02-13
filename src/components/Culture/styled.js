2de06fea7dfc95ba447ff51098652a41be1894ab
이상훈 <shlee@station3.co.kr>
2019-02-13 11:37:18 +0900

    Updates

--- src/components/Culture/styled.js
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components'
-
-export const Label = styled.h1`
-  color: #000;
-  font-weight: bold;
-  text-transform: uppercase;
-  z-index: 2;
-`
--- src/components/Home/Contact/Map/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-import Controller from './Controller'
-import { Map } from '../styled'
-
-const MapWrap = () => <Map id="map" />
-
-export default Controller(MapWrap)
